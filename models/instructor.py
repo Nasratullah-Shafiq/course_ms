cfec1b91904578e20dc4cdc11d7692b1b90ab1d2
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-10-11 08:49:52 +0430

    new changes

---
+++ models/instructor.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+class Instructor(models.Model):
+    _name = 'course.instructor'
+    _description = 'Instructor'
+
+    name = fields.Char('Instructor Name', required=True)
+    partner_id = fields.Many2one('res.partner','Partner')
+    specialization = fields.Char('Specialization')
+    course_ids = fields.One2many('course.course','instructor_id','Courses')
