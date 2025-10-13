28462f01998044ec117a6d025c37f01d98f67fd6
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-10-13 22:53:54 +0430

    new changes

--- models/enrollment.py
+++ models/enrollment.py
@@ -1,5 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
+
+
 #
 # class Enrollment(models.Model):
 #     _name = 'course.enrollment'
@@ -17,8 +19,6 @@ from odoo.exceptions import ValidationError
 #             existing = self.search([('student_id','=',record.student_id.id),('course_id','=',record.course_id.id),('id','!=',record.id)])
 #             if existing:
 #                 raise ValidationError('Student already enrolled in this course.')
-
-
 
 
 class CourseEnrollment(models.Model):
@@ -41,11 +41,9 @@ class CourseEnrollment(models.Model):
         ('student_course_unique', 'unique(student_id,course_id)', 'Student is already enrolled in this course')
     ]
 
-
     def action_mark_completed(self):
         for rec in self:
             rec.state = 'completed'
-
 
     def action_cancel(self):
         for rec in self:
@@ -53,42 +51,39 @@ class CourseEnrollment(models.Model):
                 raise ValidationError(_('Cannot cancel enrollment when invoice is posted. Cancel invoice first.'))
                 rec.state = 'cancelled'
 
-
     def action_create_invoice(self):
         """Create an account.move invoice for the enrollment's course fee."""
 
-
-    AccountMove = self.env['account.move']
-    for rec in self:
-        if not rec.course_id or not rec.course_id.fee:
-            raise ValidationError(_('Course has no fee defined.'))
-    # find/create partner
-    partner = rec.student_id.partner_id
-    if not partner:
-        partner = self.env['res.partner'].create({'name': rec.student_id.name})
-    rec.student_id.partner_id = partner
-
-    invoice_vals = {
-        'partner_id': partner.id,
-        'move_type': 'out_invoice',
-        'invoice_date': fields.Date.context_today(self),
-        'invoice_line_ids': [(0, 0, {
-            'name': rec.course_id.name,
-            'quantity': 1,
-            'price_unit': rec.course_id.fee,
-            'product_id': False,
-        })]
-    }
-    invoice = AccountMove.create(invoice_vals)
-    rec.invoice_id = invoice.id
-    rec.message_post(body=_('Invoice created: %s') % (invoice.name or invoice.id,))
-
-
-    def action_register_payment(self):
+        AccountMove = self.env['account.move']
         for rec in self:
-            if not rec.invoice_id:
-                raise ValidationError(_('No invoice linked. Please create invoice first.'))
+            if not rec.course_id or not rec.course_id.fee:
+                raise ValidationError(_('Course has no fee defined.'))
 
-
-    if rec.invoice_id.state != 'posted':
-        rec.invoice_id.action_post()
+    # find/create partner
+    # partner = rec.student_id.partner_id
+    #     if not partner:
+    #     partner = self.env['res.partner'].create({'name': rec.student_id.name})
+    #     rec.student_id.partner_id = partner
+    #
+    # invoice_vals = {
+    #     'partner_id': partner.id,
+    #     'move_type': 'out_invoice',
+    #     'invoice_date': fields.Date.context_today(self),
+    #     'invoice_line_ids': [(0, 0, {
+    #         'name': rec.course_id.name,
+    #         'quantity': 1,
+    #         'price_unit': rec.course_id.fee,
+    #         'product_id': False,
+    #     })]
+    # }
+    # invoice = AccountMove.create(invoice_vals)
+    # rec.invoice_id = invoice.id
+    # rec.message_post(body=_('Invoice created: %s') % (invoice.name or invoice.id,))
+    #
+    # def action_register_payment(self):
+    #     for rec in self:
+    #         if not rec.invoice_id:
+    #             raise ValidationError(_('No invoice linked. Please create invoice first.'))
+    #
+    # if rec.invoice_id.state != 'posted':
+    #     rec.invoice_id.action_post()
